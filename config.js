7313e7cde48e699556aa3849a961818e8d186542
Lontsi Genie <dimitrilontsi19@gmail.com>
2025-07-06 13:08:03 +0100

    Mise à jour admin.html et config.js pour JSONBin

--- config.js
+++ config.js
@@ -1,8 +1,21 @@
 // config.js
-// Cette version va chercher le mot de passe depuis password.json
+// Version avec JSONBin.io (accès dynamique)
+
+const JSONBIN_URL = "https://api.jsonbin.io/v3/b/686A5BBA8960C979A5B8018B/latest";
+const JSONBIN_API_KEY = "$2a$10$1FmC391QqDBD0/g6KInxuMT1WapHd2XM1D2KAH8HOU6cDHL5h1ce";
 
 async function getPassword() {
-  const response = await fetch('password.json');
-  const data = await response.json();
-  return data.password;
+  try {
+    const response = await fetch(JSONBIN_URL, {
+      headers: {
+        'X-Master-Key': JSONBIN_API_KEY
+      }
+    });
+
+    const data = await response.json();
+    return data.record.password;
+  } catch (error) {
+    console.error("Erreur lors de la récupération du mot de passe :", error);
+    return null;
+  }
 }
